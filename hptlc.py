76d7af6e6991769b69cd4a554b5f6ae47d572e83
Etienne Russeil <etienne@clrlsstpo12w.in2p3.fr>
2024-11-29 14:45:14 +0100

    Deals with windows path for curve plot

--- hptlc.py
+++ hptlc.py
@@ -58,7 +58,7 @@ class HPTLC_extracter():
             rectangle = image[bottom:top:-1, center - half_window : center + half_window, :3]
             averaged = np.mean(rectangle, axis=1)
             all_samples.append(averaged)
-
+os.path.normpath(path)
             if save:
                 if not os.path.isdir('single_hptlc'):
                     os.makedirs('single_hptlc')
@@ -116,7 +116,7 @@ def show_curve():
         for i in range(3):
             plt.plot(curve[:, i], color=colors[i])
 
-        plt.title(path)
+        plt.title(os.path.normpath(path))
         plt.ylim(0, 255)
         plt.show()
 
