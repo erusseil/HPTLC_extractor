7f79f040c223f6351a7384988c8cc2f253a15701
Etienne Russeil <etienne@clrlsstpo12w.in2p3.fr>
2024-12-03 11:26:00 +0100

    Added proper normalization of the data. Raw or norm data are stored

--- hptlc.py
+++ hptlc.py
@@ -35,6 +35,12 @@ class HPTLC_extracter():
         if not os.path.isdir(HPTLC_extracter.main_folder_path):
             os.makedirs(HPTLC_extracter.main_folder_path)
 
+        if not os.path.isdir(f"{HPTLC_extracter.main_folder_path}/raw/"):
+            os.makedirs(f"{HPTLC_extracter.main_folder_path}/raw/")
+
+        if not os.path.isdir(f"{HPTLC_extracter.main_folder_path}/standard/"):
+            os.makedirs(f"{HPTLC_extracter.main_folder_path}/standard/")
+
         # Create an empty dict for new objects that have not been studied yet.
         dico = {}
         for elu in HPTLC_extracter.stardard_eluants:
@@ -50,10 +56,11 @@ class HPTLC_extracter():
         json_object = json.dumps(dico, indent = 2) 
 
         for name in self.names:
-            path_name = f"{HPTLC_extracter.main_folder_path}/{name}.json"
-            if not os.path.isfile(path_name):
-                with open(path_name, "w") as outfile:
-                    outfile.write(json_object)
+            for folder in ['raw', 'standard']:
+                path_name = f"{HPTLC_extracter.main_folder_path}/{folder}/{name}.json"
+                if not os.path.isfile(path_name):
+                    with open(path_name, "w") as outfile:
+                        outfile.write(json_object)
 
     @staticmethod
     def convert_image_to_array(path, length, X_offset, Y_offset, front, inter_spot_dist, names, save=False):
@@ -91,9 +98,9 @@ class HPTLC_extracter():
                                                  self.front, self.inter_spot_dist,
                                                  self.names)
 
-        
+        # For the raw data
         for idx, sample in enumerate(all_sample):
-            save_path = f"{HPTLC_extracter.main_folder_path}{self.names[idx]}.json"
+            save_path = f"{HPTLC_extracter.main_folder_path}/raw/{self.names[idx]}.json"
 
             # Read previous already existing data
             with open(save_path, 'r') as openfile:
@@ -107,6 +114,70 @@ class HPTLC_extracter():
             json_dico = json.dumps(json_object, indent = 2) 
             with open(save_path, "w") as outfile:
                 outfile.write(json_dico)
+
+        #Same for the normalized data
+        for idx, sample in enumerate(all_sample):
+
+            norm_sample = self.normalize(sample)
+            save_path = f"{HPTLC_extracter.main_folder_path}/standard/{self.names[idx]}.json"
+
+            # Read previous already existing data
+            with open(save_path, 'r') as openfile:
+                json_object = json.load(openfile)
+
+            # Add or replace with the new info
+            for idx2, channel in enumerate(['R', 'G', 'B']):
+                json_object[self.eluant][self.observation][channel] = list(norm_sample[:, idx2])
+
+            # Save again
+            json_dico = json.dumps(json_object, indent = 2) 
+            with open(save_path, "w") as outfile:
+                outfile.write(json_dico)
+
+    @staticmethod
+    def normalize(sample):
+
+        from scipy.signal import find_peaks
+        from pybaselines import Baseline
+
+        sign = HPTLC_extracter.peak_or_holes(sample)
+        all_peaks = []
+        norm_sample = []
+        
+        for i in range(3):
+            baseline_fitter = Baseline()
+            bkg, _ = baseline_fitter.asls(sign * sample[:, i], lam=1e7)
+            new = sign * sample[:, i] - bkg
+            norm_sample.append(new)
+        
+            arg_peaks = find_peaks(new, prominence=3)
+            all_peaks.append(new[arg_peaks[0]])
+        
+        flatten_peaks = [item for row in all_peaks for item in row]
+        
+        # We normalize for the absolute maximum peak to be equal to 1.
+        if len(flatten_peaks)>0:
+            norm_factor = max(flatten_peaks)
+        
+        # If no peak is detected, we don't normalize in order to not overfit the noise.
+        else:
+            norm_factor = 1
+
+        norm_sample = sign * np.array(norm_sample).T/norm_factor
+        return norm_sample
+
+    @staticmethod
+    def peak_or_holes(sample):
+        shifted = sample - np.median(sample, axis=0)
+        summed = np.sum(shifted)
+    
+        # If most of the signal is above the median, it is peaks
+        if summed>0:
+            return 1
+    
+        # Else most of the data is below, it is holes
+        else:
+            return -1
 
 
 def main():
@@ -133,18 +204,21 @@ def show_curve():
 
     import config
     import matplotlib.pyplot as plt 
-
     
     for path in config.show:
+
+        # Read previous already existing data
+        with open(path, 'r') as openfile:
+            json_object = json.load(openfile)
         
-        curve = np.loadtxt(os.path.normpath(path), delimiter=",")
         colors = ['r', 'g', 'b']
+        RGB = ['R', 'G', 'B']
 
         for i in range(3):
-            plt.plot(curve[:, i], color=colors[i])
+            curve = json_object[config.eluant][config.observation][RGB[i]]
+            plt.plot(curve, color=colors[i])
 
         plt.title(path)
-        plt.ylim(0, 255)
         plt.show()
 
     
