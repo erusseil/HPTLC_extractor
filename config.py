2acf77b17dff1d4d3925a39d7476c6e0183b691b
erusseil <79919110+erusseil@users.noreply.github.com>
2024-12-08 21:49:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ X_offset = 22.0 #Distance from the left of the plate, to the center of the first
 Y_offset = 8.0 #Distance from the bottom of the plate, to the center of the spots
 inter_spot_dist = 12.0 #Distance between two spot centers
 
-# Names of the products. They should be in the same order as deposited on the plate.
+# Names of the products. They should be in the same order as deposited on the plate. One of them MUST be an empty string. 
+It indicates the place of the empty track that will be used to remove the background.
+
 names = ['sample_0',
  'sample_1',
  'sample_2',
@@ -13,7 +15,7 @@ names = ['sample_0',
  'sample_4',
  'sample_5',
  'sample_6',
- 'sample_7',
+ '',
  'sample_8',
  'sample_9',
  'sample_10',
@@ -25,15 +27,16 @@ names = ['sample_0',
 path = '../images/Data test p-anisaldehyde Visible.png'
 
 # Scientific variables
-# NB: This is only required for the production of the standardize database, i.e. using main()
+# Eluant to choose among: ['LPDS', 'MPDS', 'HPDS']
 eluant = 'LPDS'
+
+# Observing setup to choose among: ['254nm', '366nm', 'visible', 'developer']
 observation = 'visible'
 
 # Curve visualization, i.e. using show_curve()
 #NB: A list of files can be input for multiple visualization at once
 show = ['single_hptlc/sample_1.csv',
-       'single_hptlc/sample_4.csv',
-       'single_hptlc/sample_10.csv']
+       'single_hptlc/sample_4.csv']
 
-compute_distances = ["sample_2", "sample_3", 'sample_4','sample_5','sample_6']
+compute_distances = ["sample_2", "sample_3"]
 show_n_best = 5
