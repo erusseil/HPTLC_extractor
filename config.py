775ddd974418ae9a3ebe883b6adf964cf3741939
erusseil <etiennerusseil63@gmail.com>
2024-12-19 12:18:12 +0100

    Updated the config, fixed minor issues and updated requirement

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 #Setup of the machine
 length = 200.0 #Length of the plate
 front = 70.0 #Distance from the spot to the front
-X_offset = 22.0 #Distance from the left of the plate, to the center of the first spot
+X_offset = 20.9 #Distance from the left of the plate, to the center of the first spot
 Y_offset = 8.0 #Distance from the bottom of the plate, to the center of the spots
-inter_spot_dist = 12.0 #Distance between two spot centers
+inter_spot_dist = 11.3 #Distance between two spot centers
 
-# Names of the products. They should be in the same order as deposited on the plate. One of them MUST be an empty string. 
-It indicates the place of the empty track that will be used to remove the background.
+# Names of the products. They should be in the same order as deposited on the plate. One of them MUST be an empty string.
+# It indicates the place of the empty track that will be used to remove the background.
 
 names = ['sample_0',
  'sample_1',
@@ -21,10 +21,11 @@ names = ['sample_0',
  'sample_10',
  'sample_11',
  'sample_12',
- 'sample_13']
+ 'sample_13',
+ 'sample_14']
 
-# Path to the image
-path = '../images/Data test p-anisaldehyde Visible.png'
+# Path to the image (must leave the r before the string if using Windows path)
+path = r'C:\Users\Haris\Desktop\HPTLC_extractor\Images\LPDS_visible.png'
 
 # Scientific variables
 # Eluant to choose among: ['LPDS', 'MPDS', 'HPDS']
@@ -35,8 +36,7 @@ observation = 'visible'
 
 # Curve visualization, i.e. using show_curve()
 #NB: A list of files can be input for multiple visualization at once
-show = ['single_hptlc/sample_1.csv',
-       'single_hptlc/sample_4.csv']
+show = []
 
 compute_distances = ["sample_2", "sample_3"]
 show_n_best = 5
