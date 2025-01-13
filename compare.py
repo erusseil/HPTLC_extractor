3328687b4fb1ddb8a8834245a631fb924990212a
erusseil <etiennerusseil63@gmail.com>
2025-01-13 10:57:17 +0100

    Add exception of null graph

--- compare.py
+++ compare.py
@@ -30,16 +30,16 @@ def mesure_distances(main_folder_path, name):
             for obs in main_object[elu]:
                 main_data = main_object[elu][obs]
                 other_data = other_object[elu][obs]
-                
+
                 if (other_data['R'] != []) & (main_data['R'] != []):
-    
+
                     other_to_compare = np.array([other_data['R'],
                                                     other_data['G'],
                                                     other_data['B']])
                     main_to_compare = np.array([main_data['R'],
                                                     main_data['G'],
                                                     main_data['B']])
-                    
+
                     distances.append(compute_single_distance_dtw(np.array(main_to_compare), np.array(other_to_compare)))
 
                 else:
@@ -66,14 +66,14 @@ def mesure_distances(main_folder_path, name):
     warnings.filterwarnings(action='ignore', message='Mean of empty slice')
     col_normed = df / np.nanmean(df, axis=0)
     norm_mean = np.nanmean(col_normed, axis=1)
-    
+
     to_dump['Normalized distance'] = np.round(norm_mean, 4)
     to_dump['Mean distance'] = np.round(np.nanmean(df, axis=1), 4)
     to_dump = to_dump.sort_values("Normalized distance")
     to_dump = to_dump[['Name', "Normalized distance", "Mean distance"] + all_col_names]
     save_name = main_folder_path + '/distances/' + name[:-5] if name[-5:]=='.json' else name
     to_dump.to_csv(save_name + ".csv", index=False)
-    
+
 
 def compute_single_distance(data1, data2):
     diff = abs(data1 - data2)
@@ -89,8 +89,8 @@ def compute_single_distance_dtw(data1, data2):
         for i in range(3):
             # Find the dtw map mapping because data 1 and 2. Then use it to compute the squared difference of the two remapped profiles.
             # Because the mapping of data 1 to data 2 is not exactly the same as data 2 to data 1, we do it twice and average.
-        
-            dtw_map = dtw.dtw(datas[order[0]][i], datas[order[1]][i], keep_internals=True, 
+
+            dtw_map = dtw.dtw(datas[order[0]][i], datas[order[1]][i], keep_internals=True,
                 step_pattern=dtw.rabinerJuangStepPattern(6, "c"))
             ds.append(np.mean(np.square(datas[order[0]][i][dtw_map.index1] - datas[order[1]][i][dtw_map.index2])))
 
@@ -126,39 +126,43 @@ def produce_full_graph(main_folder_path, thresh):
     m = m.astype(int)
 
     G = nx.from_numpy_array(m)
-    
+
     edge_weights = []
     # Iterate over the nodes and add weights from the distance matrix
     for i in list(G.nodes):
         for j in list(G[i]):
             if (m[i, j] != 0) & (i<j):  # Look only if there is an egde on all of the matrix (because it is symmetrical)
                 edge_weights.append(1/(m_contiunous[i, j]))  # Assign distance as edge weight
-    
-    # The scaling is temporary and a better one should be chosen once more data is available
-    min_weight = min(edge_weights)
-    max_weight = max(edge_weights)
-    scaled_weights = [0.5 + 5 * (weight - min_weight) / (max_weight - min_weight) for weight in edge_weights]
 
-    plot_distance_graph(G, dict(matrix['Unnamed: 0']), scaled_weights, main_folder_path + 'distances/analysis/summary_graph.png')
+    if edge_weights == []:
+        print("The linking threshold is too low or the samples are too different. No link have a created. No graph has been computed")
+
+    else:
+        # The scaling is temporary and a better one should be chosen once more data is available
+        min_weight = min(edge_weights)
+        max_weight = max(edge_weights)
+        scaled_weights = [0.5 + 5 * (weight - min_weight) / (max_weight - min_weight) for weight in edge_weights]
+
+        plot_distance_graph(G, dict(matrix['Unnamed: 0']), scaled_weights, main_folder_path + 'distances/analysis/summary_graph.png')
 
 
 def plot_distance_graph(G, labels, scaled_weights, save_path):
 
-    import matplotlib.pyplot as plt 
+    import matplotlib.pyplot as plt
 
     pos = nx.forceatlas2_layout(G, seed=42, strong_gravity=True)  # positions for all nodes
-    
+
     plt.figure(figsize=(10,10))
     nx.draw(G, pos=pos)
-    
+
     # nodes
     options = {"edgecolors": "tab:gray", "node_size": 1200, "alpha": 0.9}
     nx.draw_networkx_nodes(G, pos, **options)
-    
+
     # edges
     nx.draw_networkx_edges(G, pos, width=scaled_weights, alpha=1)
     nx.draw_networkx_labels(G, pos, labels, font_size=16, font_color="whitesmoke")
-    
+
     plt.axis("off")
     plt.savefig(save_path)
     plt.show()
@@ -167,7 +171,7 @@ def compute_all_distances():
 
     main_folder_path = hptlc.HPTLC_extracter.main_folder_path
     path = main_folder_path + '/standard/'
-    
+
     all_files = [f for f in listdir(path) if isfile(join(path, f))]
     for name in all_files:
             mesure_distances(main_folder_path, name)
@@ -177,16 +181,16 @@ def matrix_and_graph():
     main_folder_path = hptlc.HPTLC_extracter.main_folder_path
     path = main_folder_path + '/distances/'
     all_files = [f for f in listdir(path) if isfile(join(path, f))]
-    
+
     dfs = []
     for file in all_files:
         dfs.append(pd.read_csv(path + file))
-        
+
     sample_names = [k[:-4] for k in all_files]
-    
+
     # Create an empty dictionary to hold the final distance matrix
     distance_matrix = {name: {name: 0 for name in sample_names} for name in sample_names}
-    
+
     # Iterate over each dataframe and populate the distance matrix
     for idx, df in enumerate(dfs):
         sample_name = sample_names[idx]
@@ -195,7 +199,7 @@ def matrix_and_graph():
             distance = row['Normalized distance']
             distance_matrix[sample_name][other_sample] = distance
             distance_matrix[other_sample][sample_name] = distance  # for symmetry
-    
+
     # Convert the dictionary to a DataFrame
     distance_df = pd.DataFrame(distance_matrix)
     distance_df.to_csv(main_folder_path + '/distances/analysis/summary_matrix.csv')
@@ -203,13 +207,11 @@ def matrix_and_graph():
     thresh = config.threshold_graph
     produce_full_graph(main_folder_path, thresh)
 
-    
+
 def main():
-    
+
     main_folder_path = hptlc.HPTLC_extracter.main_folder_path
 
     for name in config.compute_distances:
         mesure_distances(main_folder_path, name)
         show_results(main_folder_path, name, n=config.show_n_best)
-
-        
